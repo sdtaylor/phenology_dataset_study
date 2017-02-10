c2ded49bd436574c55a4ee18deecaf6cbd964347
Shawn <shawntaylor@weecology.org>
2017-02-10 10:41:49 -0500

    take out GDD and NCD calculations
    These are now done in the full model

--- compile_harvard_data.R
+++ compile_harvard_data.R
@@ -3,33 +3,23 @@ library(lubridate)
 
 
 data_dir = '~/data/harvard_forest/'
+
+
 plant_info = read_csv(paste0(data_dir, 'hf003-01-plant.csv')) %>%
   select(tree.id, species=latin) %>%
   mutate(species=tolower(species))
 observations = read_csv(paste0(data_dir, 'hf003-03-spring.csv')) %>%
   mutate(year=year(date)) %>%
   rename(doy=julian) 
-#temperature_data_local = read_csv(paste0(data_dir, 'hf004-02-filled.csv')) %>%
-#  mutate(date = round_date(datetime, 'day')) %>%
-#  group_by(date) %>%
-#  summarize(temp = mean(ta.2.5m.filled, na.rm=T)) %>%
-#  mutate(year=year(date), doy=yday(date))
-
-#Downloaded from the PRISM website. tmean for 1990-01-01 - 2016-12-32 for Latitude: 42.5429   Longitude: -72.2011
-temperature_data = read_csv(paste0(data_dir, 'PRISM_Harvard_forest_tmean.csv'), skip = 10)
-colnames(temperature_data) = c('date','temp')
-temperature_data = temperature_data %>%
-  mutate(year=year(date), doy=yday(date))
 
 #For each tree and year, get the  first day that
 #Bud Break is > 50%
 observations = observations %>%
   filter(bcon=='BB', bbrk > 50) %>%
-  select(date, doy, tree.id, year) %>%
+  select(doy, tree.id, year) %>%
   group_by(tree.id, year) %>%
   top_n(1, -doy) %>%
   ungroup()
-
 
 
 #Some individual tree.id codes don't have a species name in plant_info. 
@@ -45,32 +35,32 @@ observations = observations %>%
   left_join(species_codes, by='tree.id') %>%
   select(-tree.id)
 
-######################################################################################
-#Calculate growing and chilling degree days
-
-
-get_degree_days = function(year_to_get, doy_to_get, deg_day_threshold=0){
-  temp = temperature_data %>%
-    filter(year==year_to_get, doy<=doy_to_get) %>%
-    filter(temp>deg_day_threshold)
-  #tots=sum(temp$temp, na.rm=T)
-  #if(is.na(tots)) print(paste0(year_to_get,'-',doy_to_get,'-',tots))
-  return(sum(temp$temp, na.rm=T))
-}
-
-get_chill_days = function(year_to_get, doy_to_get, chill_threshold=0){
-  temp = temperature_data %>%
-    filter(year==year_to_get, doy<=doy_to_get) %>%
-    mutate(is_chill_day = ifelse(temp<chill_threshold, 1,0))
-  #tots=sum(temp$is_chill_day, na.rm=T)
-  #if(is.na(tots)) print(paste0(year_to_get,'-',doy_to_get,'-',tots))
-  return(as.numeric(sum(temp$is_chill_day, na.rm=T)))
-}
-
-
+#Don't want trees not identified to species
 observations = observations %>%
-  rowwise() %>%
-  mutate(GDD=get_degree_days(year_to_get = year, doy_to_get = doy), NCD=get_chill_days(year_to_get = year, doy_to_get = doy)) %>%
-  ungroup()
+  filter(!species %in% c("amelanchier sp", "crataegus sp", "aronia sp", "rhododendron sp"))
 
 write_csv(observations, './cleaned_data/harvard_observations.csv')
+
+#########################################################
+#Downloaded from the PRISM website. tmean for 1990-01-01 - 2016-12-32 for Latitude: 42.5429   Longitude: -72.2011
+temperature_data = read_csv(paste0(data_dir, 'PRISM_Harvard_forest_tmean.csv'), skip = 10)
+colnames(temperature_data) = c('date','temp')
+temperature_data = temperature_data %>%
+  mutate(year=year(date), doy=yday(date)) 
+
+#Limit temp data to fall and mid summer
+temperature_data = temperature_data %>%
+  filter(doy <=180 | doy >= 240)
+
+#Assigne fall temp to the next years growing season
+#Set jan 1 as doy 0, anything before that as negative doy's
+temperature_data = temperature_data %>%
+  mutate(year = ifelse(doy>=240, year+1, year)) %>%
+  mutate(base_date = as_date(paste0(year,'-01-01'))) %>%
+  mutate(doy = date - base_date) %>%
+  select(-date, -base_date)
+
+write_csv(temperature_data, './cleaned_data/harvard_temp.csv')
+
+
+
--- compile_npn_data.R
+++ compile_npn_data.R
@@ -3,74 +3,54 @@ library(lubridate)
 
 data_dir = '~/data/phenology/'
 
-site_info = read_csv(paste0(data_dir,'site_data.csv')) %>%
-  select(Site_ID, Latitude, Longitude)
-
-observations = read_csv(paste0(data_dir,'observations_spp_of_interest.csv')) %>%
-  filter(Phenophase_ID == 371, Phenophase_Status==1) %>%
-  select(date=Observation_Date, Site_ID, species) %>%
-  mutate(year = year(date), doy=yday(date))
-
+################################################################
 temperature_data = read_csv(paste0(data_dir,'site_PRISM_values.csv')) %>%
   mutate(date = as.Date(as.character(date), '%Y%m%d')) %>%
   mutate(year = year(date), doy = yday(date)) %>%
   rename(temp=value) %>%
   select(-var)
 
-##############################################################################
-#Compile degree days and chill days for each year
+#Limit temp data to fall and mid summer
+temperature_data = temperature_data %>%
+  filter(doy <=180 | doy >= 240)
 
-compile_deg_days = function(df){
-  if(length(unique(df$year))>1) stop('>1 year')
-  df = df %>%
-    arrange(doy)
-  
-  df$is_chill_day = ifelse(df$temp<=0, 1, 0)
-  df$deg_day      = ifelse(df$temp>0, df$temp, 0)
-  df$NCD = 0
-  df$GDD = 0
-  
-  for(i in 1:nrow(df)){
-    df$NCD[i] = sum(df$is_chill_day[1:i])
-    df$GDD[i] = sum(df$deg_day[1:i])
-    
-  }
-  
-  df = df %>% 
-    select(-is_chill_day, -deg_day)
-  
-}
+#Assigne fall temp to the next years growing season
+#Set jan 1 as doy 0, anything before that as negative doy's
+temperature_data = temperature_data %>%
+  mutate(year = ifelse(doy>=240, year+1, year)) %>%
+  mutate(base_date = as_date(paste0(year,'-01-01'))) %>%
+  mutate(doy = date - base_date) %>%
+  select(-date, -base_date)
 
-temp_data_with_gdd_file = paste0(data_dir,'site_PRISM_values_gdd_ncd.csv')
-if(file.exists(temp_data_with_gdd_file)){
-  temperature_data = read_csv(temp_data_with_gdd_file)
-} else {
-  temperature_data = temperature_data %>% 
-    group_by(Site_ID, year) %>%
-    do(compile_deg_days(.))
-  write_csv(temperature_data, temp_data_with_gdd_file)
-}
+write_csv(temperature_data, './cleaned_data/npn_temp.csv')
 
 ######################################################################################
-#mean_spring_temperature = temperature_data %>%
-#  filter(doy<60) %>%
-#  group_by(Site_ID, year) %>%
-#  summarize(mean_spring_temp = mean(value))
+site_info = read_csv(paste0(data_dir,'site_data.csv')) %>%
+  select(Site_ID, Latitude, Longitude)
+
+observations = read_csv(paste0(data_dir,'observations_spp_of_interest.csv')) %>%
+  filter(Phenophase_ID == 371, Phenophase_Status>=0) %>%
+  select(date=Observation_Date, Site_ID, species, Phenophase_Status) %>%
+  mutate(year = year(date), doy=yday(date))
+
+#site,year,species where a budbreak==0 was the first observation in a year
+budbreak_0 = observations %>%
+  group_by(species, Site_ID, year) %>%
+  top_n(1, -doy) %>%
+  filter(Phenophase_Status==0) %>%
+  select(species, Site_ID, year) %>%
+  mutate(keep='yes')
 
 observations = observations %>%
-  left_join(select(temperature_data, Site_ID, year, doy, NCD, GDD), by=c('Site_ID','year','doy'))
-rm(temperature_data, temp_data_with_gdd_file)
-
-#Unlikely that people are observing true bud break past august, or in the first few days of Jan
-#when no GDD has accumulated.
+  filter(Phenophase_Status==1) %>%
+  left_join(budbreak_0, by=c('species','Site_ID','year')) %>%
+  filter(keep=='yes') %>%
+  select(-keep, -Phenophase_Status, -date)
+  
+#Unlikely that people are observing true bud break past august
 #TODO: be more systematic about this.
 observations = observations %>%
   filter(doy<240)
-
-#Some sites are missing GDD and NCD data, likely because they're near a large water body
-#and don't have a prism cell. 
-observations = observations[complete.cases(observations),]
-
 
 write_csv(observations, './cleaned_data/NPN_observations.csv')
 
