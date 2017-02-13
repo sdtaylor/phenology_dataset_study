766328d02389e53b7f08f68a256fac93606b1782
Shawn <shawntaylor@weecology.org>
2017-02-13 13:04:21 -0500

    comparing parameters from chuine models now instead of running the models themselves

--- compare_parameters.R
+++ compare_parameters.R
@@ -1,78 +1,26 @@
 library(tidyverse)
-library(broom)
+#library(broom)
+
+npn_results = read_csv('./results/npn_results.csv')
+
+#Some NPN species did not have adqequate sample size, filter
+#harvar species to ones that did
+harvard_results = read_csv('./results/harvard_results.csv') %>%
+  filter(species %in% unique(npn_results$species)) %>%
+  select(-t1_slope)
+
+all_results = harvard_results %>%
+  bind_rows(npn_results)
+
+all_results = all_results %>%
+  gather(parameter, value, -boostrap_num, -dataset, -species) 
+
+#all_results = all_results %>%
+#  filter(species %in% c('acer rubrum', 'vaccinium corymbosum'))
+
+ggplot(all_results, aes(species, value))+
+  geom_boxplot(position = 'dodge', outlier.color = NA, aes(fill=dataset)) +
+  facet_wrap(~parameter, scales = 'free', nrow=5)
 
 
-harvard_observations = read_csv('./cleaned_data/harvard_observations.csv')
-npn_observations = read_csv('./cleaned_data/NPN_observations.csv')
-
-
-
-################################################################
-get_param_estimates = function(df){
-  #Do a lm estimate for the starting values before fitting the nls()
-  #http://stats.stackexchange.com/questions/160552/why-is-nls-giving-me-singular-gradient-matrix-at-initial-parameter-estimates
-  
-  a.0 = min(df$GDD)*0.5
-  
-  m.0 = lm(log(GDD-a.0) ~ NCD, data=df)
-  start_list = list(b=exp(coef(m.0)[1]), c=coef(m.0)[2], a=a.0)
-  m = nls(GDD ~ a + b*exp(c*NCD), data=df, start=start_list, control = nls.control(maxiter = 500))
-  return(broom::tidy(m))
-}
-#############################################################
-
-
-parameter_esimates=data.frame()
-#Models from harvard forest data
-for(this_species in unique(harvard_observations$species)){
-  this_species_data = harvard_observations %>%
-    filter(species==this_species)
-  
-  params = try(get_param_estimates(this_species_data))
-  if(class(params)=='try-error'){
-    print(paste0('nls error: ',this_species))
-  } else {
-    params$species=this_species
-    params$data_source='harvard'
-    parameter_esimates = parameter_esimates %>%
-      bind_rows(params)
-  }
-}
-
-#Models from NPN data
-for(this_species in unique(npn_observations$species)){
-  this_species_data = npn_observations %>%
-    filter(species==this_species) 
-    
-  params = try(get_param_estimates(this_species_data))
-  if(class(params)=='try-error'){
-    print(paste0('nls error: ',this_species))
-  } else {
-    params$species=this_species
-    params$data_source='npn'
-    parameter_esimates = parameter_esimates %>%
-      bind_rows(params)
-  }
-}
-
-############################################################
-#Drop any species that don't have both data sources
-#TODO: explain why I can't compare all the species. data deficient, model won't converge, etc.
-species_to_model = parameter_esimates %>%
-  filter(data_source=='npn') %>%
-  select(species) %>%
-  distinct()
-
-parameter_esimates = parameter_esimates %>%
-  filter(species %in% species_to_model$species)
-
-ggplot(parameter_esimates, aes(x=species, y=estimate, group=data_source, fill=data_source)) +
-  geom_bar(position='dodge', stat='identity') +
-  geom_errorbar(aes(ymax=estimate+std.error, ymin=estimate-std.error))+
-  facet_grid(term~., scales = 'free_y')
-
-ggplot(parameter_esimates, aes(x=species, y=estimate, group=data_source, fill=data_source)) +
-  geom_bar(position='dodge', stat='identity') +
-  geom_errorbar(aes(ymax=estimate+std.error, ymin=estimate-std.error))+
-  facet_grid(term~., scales = 'free_y')
 
