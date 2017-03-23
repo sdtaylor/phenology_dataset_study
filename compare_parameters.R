38a4fff9a90246869b93347f89b5c3eef94cc74e
Shawn <shawntaylor@weecology.org>
2017-03-23 16:03:00 -0400

    switch to using the combined all results file

--- compare_parameters.R
+++ compare_parameters.R
@@ -1,17 +1,17 @@
 library(tidyverse)
 #library(broom)
 
-npn_results = read_csv('./results/npn_results.csv') 
-
-#Some NPN species did not have adqequate sample size, filter
-#harvar species to ones that did
-harvard_results = read_csv('./results/harvard_results.csv') %>%
-  filter(species %in% unique(npn_results$species)) %>%
-  select(-t1_slope)
-
-all_results = harvard_results %>%
-  bind_rows(npn_results) %>%
+all_results = read_csv('./results/all_results.csv') %>%
   rename(T1=t1_int)
+
+#Keep only species that are present in NPN dataset
+npn_species = all_results %>%
+  filter(dataset == 'npn') %>%
+  select(species) %>%
+  distinct()
+
+all_results = all_results %>% 
+  filter(species %in% npn_species$species)
 
 all_results = all_results %>%
   gather(Parameter, value, -boostrap_num, -dataset, -species) 
@@ -25,8 +25,8 @@ old_names = c('quercus rubra','quercus alba','fagus grandifolia','populus tremul
 all_results$species = factor(all_results$species, levels=old_names, labels=new_names)
 all_results$dataset = factor(all_results$dataset, levels=c('npn','harvard'), labels=c('NPN','Harvard'))
 ggplot(all_results, aes(species, value))+
-  #geom_boxplot(position = 'dodge', outlier.color = NA, aes(fill=dataset)) +
-  geom_violin(position = 'dodge', aes(fill=dataset)) +
+  geom_boxplot(position = 'dodge', outlier.color = NA, aes(fill=dataset)) +
+  #geom_violin(position = 'dodge', aes(fill=dataset)) +
   scale_fill_manual(values=c('#0072B2','#E69F00')) +
   facet_wrap(~Parameter, scales = 'free', nrow=5, labeller = label_both) + 
   theme(plot.subtitle = element_text(vjust = 1), 
