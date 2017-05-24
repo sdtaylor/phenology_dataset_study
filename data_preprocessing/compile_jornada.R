d2d634fbaf2f2c5b577f2bfefb27416b21ccb9ed
Shawn <shawntaylor@weecology.org>
2017-05-24 17:51:20 -0400

    add jornada dataset

--- data_preprocessing/compile_jornada.R
+++ data_preprocessing/compile_jornada.R
@@ -1,13 +1,29 @@
 library(tidyverse)
 library(lubridate)
 
-data_dir = '~/data/phenology/jornada'
+data_dir = './raw_data/jornada'
 
-observations = read_csv(file.path(data_dir,'JornadaStudy_287_npp_perennial_plant_phenology_0.csv'), skip = 45)
+observations = read_csv(file.path(data_dir,'JornadaStudy_287_npp_perennial_plant_phenology_0.csv'), skip = 45) %>%
+  rename(Code = SPP, doy=JD, Site=SITE, PercentNON_Rep=`PercentNON-REP`) %>%
+  mutate(date = as.Date(DATE, '%m/%d/%Y'), year=year(date))
 
-plants = read_csv(file.path(data_dir,'plant_list.csv')) %>%
-  select(Genus, Species, Code) %>%
-  distinct()
+plant_names = read_csv(file.path(data_dir,'Jornada_LTER_Plant_list.csv'))%>%
+  select(species, Code)
+
+#There are a few duplicate 4 letter species codes due to species synonyms. Rather than deal
+#with it I will just exclude them as they are rare and not in the NPN dataset for comparison
+species_code_duplicates = plant_names %>%
+  group_by(Code) %>%
+  tally() %>%
+  filter(n>1)
+
+observations = observations %>%
+  filter(!Code %in% species_code_duplicates$Code)
+
+#attach full species names. those without full names are unidentified to full species (ie. ARSP, UKGR)
+observations = observations %>%
+  left_join(plant_names, by='Code') %>%
+  filter(!is.na(species))
 
 #Metadata from the raw data file
 # COMMENT  - Comment section [*see history log refers to file: JornadaStudy_287_npp_perennial_plant_phenology.his]
@@ -18,5 +34,69 @@ plants = read_csv(file.path(data_dir,'plant_list.csv')) %>%
 #PercentFRUIT = percent of all phenophase observations that are Fruit (= Dormant count divided by count sum of all phenophases)
 
 
-o = observations %>%
-  left_join(plants, by=c('SPP'='Code'))
+process_phenology_data = function(this_species, this_phenophase){
+  if(this_phenophase==371){
+    obs_subset = observations %>%
+      mutate(status = (PercentDORMANT<75)*1)
+  } else if(this_phenophase==501){
+    obs_subset = observations %>%
+      mutate(status = (PercentFLOWER>25)*1) 
+  }
+  
+  obs_subset = obs_subset %>%
+    filter(species==this_species)
+  
+  #site,year,species where a budbreak==0 was the first observation in a year
+  phenophase_0 = obs_subset %>%
+    group_by(species, Site, year) %>%
+    top_n(1, -doy) %>%
+    ungroup() %>%
+    filter(status==0) %>%
+    select(species, Site, year, status_0_doy=doy) %>%
+    mutate(keep='yes')
+  
+  #Keep only observations that were preceded by an observation of the phenophase==0
+  obs_subset = obs_subset %>%
+    filter(status==1) %>%
+    group_by(species,Site,year) %>%
+    top_n(1,-doy) %>%
+    ungroup() %>%
+    left_join(phenophase_0, by=c('species','Site','year')) %>%
+    filter(keep=='yes') %>% 
+    select(species,Site,year, doy)
+  
+  return(obs_subset)
+}
+
+#Chose these by hand. The only non-grass with sufficent observations
+species = data.frame(species=c('prosopis glandulosa','larrea tridentata'),
+                            Phenophase_ID = c(501,501))
+
+processed_data = species %>%
+  rowwise() %>%
+  do(process_phenology_data(this_species = .$species, this_phenophase = .$Phenophase_ID))
+
+#Makes sites numerica to work in the numpy models
+site_info = read_csv('non_npn_site_info.csv') %>%
+  filter(dataset=='jornada') %>%
+  select(Site_ID, Site=note)
+
+processed_data = processed_data %>%
+  left_join(site_info, by='Site') %>%
+  select(-Site)
+
+
+write_csv(observations, './cleaned_data/jornada_observations.csv')
+
+#Append to the same file written by other scripts
+species$dataset='jornada'
+
+non_npn_species_file = './cleaned_data/non_npn_species_list.csv'
+if(file.exists(non_npn_species_file)){
+  read_csv(non_npn_species_file) %>%
+    bind_rows(species) %>%
+    distinct() %>%
+    write_csv(non_npn_species_file)
+} else {
+  write_csv(species, non_npn_species_file)
+}
---
+++ raw_data/jornada/readme.txt
@@ -0,0 +1,28 @@
+file: JornadaStudy_287_npp_perennial_plant_phenology_0.csv
+    downloaded from https://portal.lternet.edu/nis/mapbrowse?packageid=knb-lter-jrn.2100287001.44 9-May-2017
+
+file: metadata.png
+    screenshot from the metadata page https://portal.lternet.edu/nis/metadataviewer?packageid=knb-lter-jrn.2100287001.44
+    taken 19-May-2017
+    
+file: PlantList-JER_web.xls
+    downloaded from https://jornada.nmsu.edu/lter/data/species/jer-plants 9-May-2017
+    Note this is the SPP list for the experimental range, not the LTER.
+
+file: species_list_raw.txt
+    copy/pasted from https://jornada.nmsu.edu/lter/data/species/lter-plants 19-May-2017
+    this is the correct species list for the Jornada LTER phenology
+    edited using the open refine project to create Jornada_LTER_Plant_list.csv
+
+file: Jornada_LTRE_Plant_list.csv
+    created using openrefine
+
+file: Jornada_LTER_site_info.csv
+    copy/pasted from Additional Information from the NPP dataset here https://portal.lternet.edu/nis/metadataviewer?packageid=knb-lter-jrn.210425001.53
+    edited to csv by hand. They are lat long, not UTM
+    Site Location: Fifteen sites (3 in each of 5 ecosystem types) were selected for monitoring of NPP. Sites and locations summarized here: Based on UTM Zone 13, Datum WGS84 Site lat (north) long (west) 
+
+file: JornadaStudy_287_npp_perennial_plant_phenology.his
+    note, has potentially important info on observations made but not recorded when everything was dormant
+    nope, nm, when something was dormant it was recorded the same as the prior month. and it's mostly the low diversity playa (zone P) sites
+    notes from observers, from https://jornada.nmsu.edu/content/plant-phenology-transect-npp-sites
