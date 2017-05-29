fbda92a3562f50c489e379b44c33bff3d7c1213c
Shawn <shawntaylor@weecology.org>
2017-05-29 19:37:40 -0400

    process all dataset temp files in here

--- data_preprocessing/extract_prism_temps.R
+++ data_preprocessing/extract_prism_temps.R
@@ -3,6 +3,7 @@ library(sp)
 library(prism)
 library(tidyverse)
 library(stringr)
+library(lubridate)
 
 options(prism.path = "~/data/prism_daily")
 
@@ -28,12 +29,49 @@ site_info_spatial = SpatialPointsDataFrame(cbind(site_info$Longitude, site_info$
 
 prism_stacked = prism_stack(ls_prism_data())
 
+##########################################################
+#Extract raw temp data
+###########################################################
+
 extracted = as.data.frame(raster::extract(prism_stacked, site_info_spatial)) %>%
   bind_cols(site_info)
 
 extracted = extracted %>%
   tidyr::gather(filename, value, -Site_ID, -dataset) %>%
-  dplyr::mutate(date=stringr::word(filename, 5, 5, sep='_'), var=stringr::word(filename, 2, 2, sep='_')) %>%
+  dplyr::mutate(date=stringr::word(filename, 5, 5, sep='_')) %>%
   dplyr::select(-filename)
 
-write_csv(extracted, '~/data/phenology/site_PRISM_values.csv')
+##########################################################
+#Convert to format used in models
+###########################################################
+
+temperature_data = extracted %>%
+  mutate(date = as.Date(as.character(date), '%Y%m%d')) %>%
+  mutate(year = year(date), doy = yday(date))
+
+#Limit temp data to fall and mid summer
+temperature_data = temperature_data %>%
+  filter(doy <=180 | doy >= 240)
+
+#Assign fall temp to the next years growing season.
+#Set jan 1 as doy 0, anything before that as negative doy's
+temperature_data = temperature_data %>%
+  mutate(year = ifelse(doy>=240, year+1, year)) %>%
+  mutate(base_date = as_date(paste0(year,'-01-01'))) %>%
+  mutate(doy = date - base_date) %>%
+  select(-date, -base_date)
+
+#All observations only go till 2016
+temperature_data = temperature_data %>%
+  filter(year<=2016)
+
+##########################################################
+#One temperature data file per dataset
+###########################################################
+
+datasets = unique(site_info$dataset)
+
+for(this_dataset in datasets){
+  temp_filename = paste0('./cleaned_data/',this_dataset,'_temp.csv')
+  write_csv(filter(temperature_data, dataset==this_dataset), temp_filename)
+}
